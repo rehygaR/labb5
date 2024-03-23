02ea34236d3b9fe107c44bed95e993a6a3663319
rehygaR <48217982+rehygaR@users.noreply.github.com>
2024-03-23 16:18:19 +0100

    Liten update

--- src/main/BinOpButton.java
+++ src/main/BinOpButton.java
@@ -4,14 +4,15 @@ import java.awt.event.ActionEvent;
 import java.util.function.IntBinaryOperator;
 
 @SuppressWarnings("serial")
-public class BinOpButton extends CalculatorButton{
+public class BinOpButton extends CalculatorButton {
 
 	private IntBinaryOperator operator;
 	private int rightOperand;
 	private int leftOperand;
 
 	/**
-	 * Konstruktor för operandknapparna, tar emot en situation, namn på knapp samt operator.
+	 * Konstruktor för operandknapparna, tar emot en situation, namn på knapp samt
+	 * operator.
 	 * 
 	 * @param sit
 	 * @param name
@@ -21,10 +22,9 @@ public class BinOpButton extends CalculatorButton{
 	public BinOpButton(Situation sit, String name, IntBinaryOperator operator) {
 		super(sit, name);
 		this.operator = operator;
-		
 
 	}
-	
+
 	public int applyOperation() {
 		return operator.applyAsInt(leftOperand, rightOperand);
 	}
@@ -32,23 +32,33 @@ public class BinOpButton extends CalculatorButton{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		 if (sit.state == sit.state.Input1 || sit.state == sit.state.HasResult) {
-	            sit.binaryOperator = this;
-	            this.setColorRed(); // Färga knappen röd
-	        } else if (sit.state == sit.state.OpReady) {
-	        	this.setColorGray(); // Färga den gamla knappen grå
-	            sit.binaryOperator = this;
-	            this.setColorRed(); // Färga den nya knappen röd
-	        }
+//		if (sit.state == sit.state.Input1 || sit.state == sit.state.HasResult) {
+//			sit.binaryOperator = this;
+//			this.setColorRed(); // Färga knappen röd
+		if (sit.state == sit.state.OpReady) {
+			this.setColorGray(); // Färga den gamla knappen grå
+			sit.binaryOperator = this;
+			this.setColorRed(); // Färga den nya knappen röd
+		}
 		transition();
 	}
 
 	@Override
 	public void transition() {
 		// TODO Auto-generated method stub
-		switch(sit.state.toString()) {
-		case "Input1":
+		switch (sit.state.toString()) {
+		case "OpReady":
 			
+			break;
+		case "Input1":
+
+			break;
+		case "Input2":
+
+			break;
+		case "HasResult":
+
+			break;
 		}
 	}
 
